c9ad709b9dc0a6e091657af5c8bc3ba9874bb858
XinweiLyu <lxw491106884@gmail.com>
2026-01-06 23:03:14 +0800

    update invoke method

---
+++ src/main/java/com/xinwei/aiagent/demo/invoke/LangChainAiInvoke.java
@@ -0,0 +1,16 @@
+package com.xinwei.aiagent.demo.invoke;
+
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.model.chat.ChatLanguageModel;
+
+public class LangChainAiInvoke {
+
+    public static void main(String[] args) {
+        ChatLanguageModel qwenModel = QwenChatModel.builder()
+                .apiKey(TestApiKey.API_KEY)
+                .modelName("qwen-max")
+                .build();
+        String answer = qwenModel.chat("这是AI 智能体项目");
+        System.out.println(answer);
+    }
+}
